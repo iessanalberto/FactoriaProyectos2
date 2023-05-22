5da69eca5391b6dc2db40bebb4e8e1b9881f61e7
Santy Cierco <santidisturbios@gmail.com>
2023-05-22 17:38:52 +0200

    autentication confirm with email code

--- app/src/main/java/com/example/myapplication/AmplifyCognito.java
+++ app/src/main/java/com/example/myapplication/AmplifyCognito.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 import com.amplifyframework.auth.AuthUserAttributeKey;
@@ -15,16 +16,41 @@ public class AmplifyCognito {
         this.context = context;
     }
 
-    public void signUp(String email, String username, String password) {
+    //public void signUp(String email, String username, String password) {
 
+
+    public void signUp(String email, String username, String password) {
         AuthSignUpOptions options = AuthSignUpOptions.builder()
                 .userAttribute(AuthUserAttributeKey.email(), email)
                 .build();
         Amplify.Auth.signUp(username, password, options,
-                result -> Log.i("AuthQuickStart", "Result: " + result.toString()),
-                error -> Log.e("AuthQuickStart", "Sign up failed", error)
-        );
+                result -> {
+                    Log.i("AuthQuickStart", "Result: " + result.toString());
+                    loadConfirm(username);
+                },
+                error -> Log.e("AuthQuickStart", "Sign up failed", error));
+    }
+
+    public void confirm(String username, String code) {
+        Amplify.Auth.confirmSignUp(username, code,
+                result -> {
+                    Log.i("AuthQuickstart", result.isSignUpComplete() ? "Confirm signUp succeeded" : "Confirm sign up not complete");
+                    loadLogin();
+                },
+                error -> Log.e("AuthQuickstart", error.toString()));
+    }
 
 
+    public void loadLogin() {
+        Intent intent = new Intent(context, SigninActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+    private void loadConfirm(String username) {
+        Intent intent = new Intent(context, ConfirmActivity.class);
+        intent.putExtra("username", username);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
     }
 }
