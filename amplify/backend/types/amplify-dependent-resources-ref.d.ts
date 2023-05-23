107b87adec2e3ded052f914dfd09595aaa07809f
Santy Cierco <santidisturbios@gmail.com>
2023-05-23 12:26:33 +0200

    create amplify function java

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -18,6 +18,13 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
+    "FactoriaPruebasJava": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "factoriapruebas": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
